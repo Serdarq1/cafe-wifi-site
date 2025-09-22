6164deec7aff7f50f54f5f23ff0cdd13ffe82325
Serdar Akova <serdarakova@Serdars-Macbook.home>
2025-09-22 22:58:45 +0300

    update database uri

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Base(DeclarativeBase):
     pass
 
 # Connect Databse
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////var/data/cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
